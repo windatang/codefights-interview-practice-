73febc759c8562e1adcdfeadea322be51b4b15ab
Dazhi Tang <Dazhi.Tang@one.verizon.com>
2017-10-01 01:41:46 -0500

    done

--- isSubtree.js
+++ isSubtree.js
@@ -1,22 +1,3 @@
-"use strict"
-/*****************************************************************************
- *  File Name:  isSubtree.js
- *  Project: HTML5
- *  Author: JustMeet - Dazhi Tang
- *
- *  Discribe:
- *
- *  This material contains, and is a part of a computer software program
- *  which is, proprietary and confidential information owned by
- *  JustMeet Technology.
- *  The program, including this material, may not be duplicated, disclosed
- *  or reproduced in whole or in part for any purpose without the express
- *  written authorization of Verizon Corporate Technology.  All authorized
- *  reproductions must be marked with this legend.
- *
- *  Copy right 2015 JustMeet Technology, Inc.
- *  All rights reserved.
- *****************************************************************************/
 //
 // Definition for binary tree:
 // function Tree(x) {
@@ -24,38 +5,33 @@
 //   this.left = null;
 //   this.right = null;
 // }
-function isSubtree(t1, t2) {
+        function isSubtree(t1, t2) {
 
-  function findroot(t1,t2){
-    if(t2==null){
-      return true;
-    }else if(t1==null){
-      if(t2==null){
-        return true;
-      }else{
-        return false;
-      }
-    }else if((
-        t1.value == t2.value && machedTree(t1,t2)) || findroot(t1.left,t2) || findroot(t1.right,t2)){
-      return true;
-    }else{
-      return false
-    }
-  }
-  function machedTree(t1,t2){
-    if((t1 == null && t2 == null)
-      &&(t1 == null && t2==null)){
-      return true;
-    }else if( t2 == null ||
-      ((t1 != null && t2!=null)
-        && machedTree(t1.left,t2.left)
-        &&t1.value == t2.value
-        &&machedTree(t1.right,t2.right))){
-      return true;
-    }else{
-      return false;
-    }
+          function findroot(t1,t2){
+            if(t2==null){
+              return true;
+            }else if(t1==null){
+              return false;
+            }else if((
+                t1.value == t2.value && machedTree(t1,t2)) || findroot(t1.left,t2) || findroot(t1.right,t2)){
+              return true;
+            }else{
+              return false
+            }
+          }
+          function machedTree(t1,t2){
+            if(t1==null && t2 == null){
+              return true;
+            }
+            else if((t2 != null && t1 != null)
+              && machedTree(t1.left,t2.left)
+              && (t1.value === t2.value)
+              &&machedTree(t1.right,t2.right)){
+              return true;
+            }else{
+              return false;
+            }
 
-  }
-  return findroot(t1,t2)
-}
+          }
+          return findroot(t1,t2)
+        }
